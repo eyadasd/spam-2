9d4cca848fa9a7423c59afdc4e95537c7647a9a4
eyadasd <37232558+eyadasd@users.noreply.github.com>
2018-11-21 11:42:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "513709288347140097"; // ايدي السررفر
-var channel = "513709288825552907";//ايدي الروم
+var server = "514735286220095499"; // ايدي السررفر
+var channel = "514735286220095505";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
